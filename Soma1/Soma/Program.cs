4d1c130650a7d7371e34e3f5c699a117331571a2
kallyane-nascimento16 <110573013+kallyane-nascimento16@users.noreply.github.com>
2022-09-28 18:32:57 -0300

    Add files via upload

---
+++ Soma1/Soma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Soma
+{
+    public static void Main(string[] args)
+    {
+
+        int numero1;
+        int numero2;
+        double resultado=0;
+
+
+       Console.WriteLine ("eae ! vamos iniciar a sua soma de dois numeros . Digite o primeiro numero");
+       numero1 =int.Parse(Console.ReadLine());
+
+        Console.WriteLine(" Digite o segundo numero");
+        numero2 = int.Parse(Console.ReadLine());
+
+        resultado = numero1 + numero2;
+
+        Console.WriteLine(" o resultado da soma dos dois numeros é " + resultado);
+
+
+    }
+}
---
+++ Soma1/dados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class dados
+{
+    public static void Main(string[]args)
+    {
+        string nome;
+        string endereco;
+        double cpf;
+
+        Console.WriteLine("qual o seu nome?");
+        nome = Console.ReadLine();
+        Console.WriteLine("qual o seu endereço?");
+        endereco = Console.ReadLine();
+        Console.WriteLine("qual o seu CPF?");
+        cpf =int.Parse (Console.ReadLine());
+
+        Console.WriteLine("Ola! seja bem-vindo ao nosso sistema");
+        Console.WriteLine("dados do usuario:");
+        Console.WriteLine("nome:"+nome);
+        Console.WriteLine("CPF:"+cpf);
+        Console.WriteLine("endereço:"+endereco);
+
+
+
+    }
+
+}
